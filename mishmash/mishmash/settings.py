8c263f665f4d018a21f774aa43681bbf828d0747
Sanjeev Chauhan <sanjeev.chauhan@duke.edu>
2025-01-30 11:14:22 -0500

    add env vars to django build

--- mishmash/mishmash/settings.py
+++ mishmash/mishmash/settings.py
@@ -102,9 +102,9 @@ WSGI_APPLICATION = "mishmash.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
+        'NAME': config('DATABASE_NAME', default='mishmash'),
+        'USER': config('DATABASE_USER', default='root'),
+        'PASSWORD': config('DATABASE_PASSWORD', default='root'),
         'HOST': config('DATABASE_HOST', default='127.0.0.1'),
         'PORT': config('DATABASE_PORT', default='3306'),
     }
