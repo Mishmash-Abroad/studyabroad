bb8ef7d67a880b6c13fac7be9ddcd0c5caf4b047
Sanjeev Chauhan <sanjeev.chauhan@duke.edu>
2025-02-24 13:21:17 -0500

    fix homepage button

--- mishmash/frontend/src/pages/HomePage.js
+++ mishmash/frontend/src/pages/HomePage.js
@@ -211,10 +211,7 @@ const HomePage = () => {
               : 'Transform your education through global experiences with HCC Study Abroad'}
           </HeroText>
           {!user && (
-            <HeroButton onClick={() => {throw new Error("This is your first error!");}}>
-              Get Started
-            </HeroButton>
-            
+            <HeroButton onClick={() => setShowLoginModal(true)}>Get Started</HeroButton>
           )}
         </HeroContent>
       </Hero>
