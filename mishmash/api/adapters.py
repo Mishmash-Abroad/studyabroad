215c9a735cfd2de0500ca2bedad0958d75e546f0
Jason Fitzpatrick <jasonfitz01@yahoo.com>
2025-02-27 19:48:36 -0500

    Made a fix because the data is being sent in extra_data and not data.

--- mishmash/api/adapters.py
+++ mishmash/api/adapters.py
@@ -1,28 +1,34 @@
+import logging
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from allauth.account.utils import user_field
 from django.core.exceptions import ValidationError
 
+logger = logging.getLogger(__name__)
+
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     """
-    Custom adapter to properly map SSO data to user fields.
-    - Sets `username` to `dukeNetID`
-    - Sets `display_name` to `name`
+    Custom adapter to correctly map SSO data to user fields.
     """
 
     def populate_user(self, request, sociallogin, data):
         """
-        Populates user fields based on the SSO response.
+        Populates user fields based on the actual SSO response.
         """
+        logger.info(f"SSO response data: {data}")
+        
         user = super().populate_user(request, sociallogin, data)
 
-        duke_net_id = data.get("dukeNetID")
-        full_name = data.get("name")
+        extra_data = sociallogin.account.extra_data
+
+        logger.info(f"SSO extra_data: {extra_data}")
+        
+        duke_net_id = extra_data.get("dukeNetID")
+        full_name = extra_data.get("name")
 
         if not duke_net_id:
             raise ValidationError("SSO response does not contain a valid 'dukeNetID' field.")
 
-        # Set fields
-        user.username = duke_net_id  # Set username to dukeNetID
-        user.display_name = full_name if full_name else "New User"  # Set display_name to 'name'
+        user.username = duke_net_id
+        user.display_name = full_name if full_name else "New User"
 
         return user
