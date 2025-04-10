f6b6876fadac866ac8531b97034bbd051d2372b3
Russell Barton <barton5russell@gmail.com>
2025-04-09 23:12:40 -0400

    got rid of favicon stuff. Maybe Ill figure it out later

--- mishmash/frontend/src/App.js
+++ mishmash/frontend/src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { updateDocumentTitle, updateFavicon } from "./utils/brandingUtils";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -23,35 +21,6 @@ import PublicLetterUploadPage from "./pages/PublicLetterUploadPage";
 
 function App() {
   const [showLoginModal, setShowLoginModal] = useState(false);
-
-  // Fetch site branding and update document title and favicon
-  useEffect(() => {
-    const fetchBranding = async () => {
-      try {
-        // API endpoint for site branding
-        const response = await axios.get('/api/site-branding/');
-        
-        if (response.data && response.data.length > 0) {
-          const branding = response.data[0];
-          
-          // Update document title if site name exists
-          if (branding.site_name) {
-            updateDocumentTitle(branding.site_name);
-          }
-          
-          // Update favicon if logo exists
-          if (branding.logo_url) {
-            updateFavicon(branding.logo_url, branding.primary_color);
-          }
-        }
-      } catch (error) {
-        console.error('Error fetching site branding:', error);
-        // If there's an error, we'll just keep the default title and favicon
-      }
-    };
-    
-    fetchBranding();
-  }, []);
 
   return (
     <Router>
