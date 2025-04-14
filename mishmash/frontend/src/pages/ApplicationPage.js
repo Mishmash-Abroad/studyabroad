2267187182efa2aabffacdf05e4f381d5c403001
Jason Fitzpatrick <jasonfitz01@yahoo.com>
2025-04-14 09:27:21 -0400

    resolved bug where warning wasn't popping up when a student is missing prereqs

--- mishmash/frontend/src/pages/ApplicationPage.js
+++ mishmash/frontend/src/pages/ApplicationPage.js
@@ -308,6 +308,7 @@ const ApplicationPage = () => {
 
   // -------------------- EVENT HANDLERS --------------------
   const handleSubmit = async (e) => {
+    console.log("submitting...");
     e.preventDefault();
     if (
       isApplicationReadOnly() ||
@@ -362,12 +363,11 @@ const ApplicationPage = () => {
     }
 
     // Check prerequisites and warn user if prerequisites are not met
-    if (application.program.prerequisites) {
+    if (program.prerequisites) {
       if (!user.ulink_username) {
         setUlinkDialogOpen(true);
         return;
-      }
-      else if (!prereqStatus.meets_all) {
+      } else if (!prereqStatus.meets_all) {
         if (window.confirm(`You are missing the following pre-requisites for this course: ${prereqStatus.missing}. Please contact the faculty leads for this program if you wish to request an exception. Do you want to apply anyway?`)) {
           updateState({ loading: true, error: "" });
         } else {
